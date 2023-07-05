1b5fe1ea54d0b2bc2eac393d07edb60e498d018e
basit <basitafraz8@gmail.com>
2023-07-05 11:52:32 +0500

    third commit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,32 +1,50 @@
-import Link from 'next/link';
-import Logo from '/public/logo.png';
-import Image from 'next/image';
-import Wrapper from '@/components/shared/Wrapper';
+"use client";
+import Logo from "/public/logo.png";
+import Image from "next/image";
+import Wrapper from "@/components/shared/Wrapper";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+import Link from "next/link";
+import { useState } from "react";
 
+let links = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/" },
+  { name: "Courses", link: "/" },
+  { name: "Blogs", link: "/" },
+];
 
 const Header = () => {
-  return (
-    <header className='sticky top-0 bg-white'>
-      <Wrapper>
-        <div className="flex justify-between max-w-screen-xl mx-auto py-5 sticky top-0 items-center">
-          {/* logo */}
-          <div>
-            {/* <h2 className="text-xl font-bold">Panaverse Dao</h2> */}
-            <Image src={Logo} alt="panaverse dao logo" />
-          </div>
+  const [open, setOpen] = useState(false);
 
-          {/* navigation */}
-          <ul className="flex gap-6 font-medium text-black">
-            <li>
-              <Link href={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link href={"/courses"}> Courses</Link>
-            </li>
-            <li>
-              <Link href={"/blog"}> Blogs</Link>
-            </li>
-          </ul>
+  return (
+    <header className="sticky top-0 bg-white">
+      <Wrapper>
+        <div>
+          <div className="flex flex-col justify-start md:flex-row  md:justify-between max-w-screen-xl mx-auto py-5 sticky top-0 md:items-center">
+            <div className="flex justify-between">
+              <Image src={Logo} alt="panaverse dao logo" />
+              <div onClick={() => setOpen(!open)} className="right-0">
+                {open ? (
+                  <AiOutlineClose className="mt-5 md:hidden" />
+                ) : (
+                  <RxHamburgerMenu className="mt-5 md:hidden" />
+                )}
+              </div>
+            </div>
+
+            <ul
+              className={`${
+                open ? "block" : "hidden"
+              } flex flex-col gap-4 md:flex-row`}
+            >
+              {links.map((item, i) => (
+                <li key={i} className="hover:underline">
+                  <Link href={item.link}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </Wrapper>
     </header>
--- src/components/widgets/SpecializedTracks.tsx
+++ src/components/widgets/SpecializedTracks.tsx
@@ -242,58 +242,56 @@ export const Data = [
   },
 ];
 
-
 const SpecializedTracks = () => {
-
   const info =
     "After completing the first three quarters the participants will select \n one or more specialization consisting of the two courses each.";
-  const [selectedItem, setSelectedItem] = useState("WMD")
-  const selectedItemData = Data.find((item)=> item.slug === selectedItem)
-   
-    return (
-      <section className="z-[-10]">
-        <Wrapper>
-          <h1 className="text-black text-3xl font-bold">Specialized Tracks:</h1>
-          <p className="mt-6 max-w-screen-xl whitespace-pre-line">{info}</p>
-          <div className="flex flex-col gap-14 sticky z-[-10] md:flex-row">
-            {/* First Section */}
-            <div className="flex flex-col mt-6 border-2 border-grey rounded-2xl border-[#00616C] w-full self-start z-[-10] sticky top-28 md:w-[60%]">
-              <p className="text-[#00616C] mt-6 mx-12">Specialized Program</p>
-              <h4 className="text-black font-semibold mt-2 mx-12 text-2xl">
-                {selectedItemData?.title}
-              </h4>
-              <p className="text-xl mt-2 mx-12">
-                {selectedItemData?.description}
-              </p>
-              <div className="mx-12 mt-5">
-                <Link href={"#"} className="underline text-teal-800 font-bold">
-                  Learn More{" "}
-                </Link>
-              </div>
-              <div className="flex flex-col gap-3 mx-12 my-12 md:flex-row">
-                {selectedItemData?.quarters.map((item,i) => (
-                  <Card
-                    heading={item.header}
-                    description={item.Description}
-                    number={item.number}
-                    border={false}
-                    key={i}
-                  />
-                ))}
-              </div>
+  const [selectedItem, setSelectedItem] = useState("WMD");
+  const selectedItemData = Data.find((item) => item.slug === selectedItem);
+
+  return (
+    <section className="z-[-10]">
+      <Wrapper>
+        <h1 className="text-black text-3xl font-bold">Specialized Tracks:</h1>
+        <p className="mt-6 max-w-screen-xl whitespace-pre-line">{info}</p>
+        <div className="flex flex-col gap-14 z-[-10] md:flex-row">
+          {/* First Section */}
+          <div className="flex flex-col mt-6 border-2 border-grey rounded-2xl border-[#00616C] w-full self-start z-[-10] md:sticky md:top-28 md:w-[60%]">
+            <p className="text-[#00616C] mt-6 mx-12">Specialized Program</p>
+            <h4 className="text-black font-semibold mt-2 mx-12 text-2xl">
+              {selectedItemData?.title}
+            </h4>
+            <p className="text-xl mt-2 mx-12">
+              {selectedItemData?.description}
+            </p>
+            <div className="mx-12 mt-5">
+              <Link href={"#"} className="underline text-teal-800 font-bold">
+                Learn More{" "}
+              </Link>
             </div>
-            {/* Second Section */}
-            <div className="flex flex-col flex-1 gap-4 gap-x-2 gap-y-4 overflow-hidden">
-              {Data.map((item, i) => (
-                <div key={i} onClick={() => setSelectedItem(item.slug)}>
-                  <Program title={item.header} program={item.title} />
-                </div>
+            <div className="flex flex-col gap-3 mx-12 my-12 md:flex-row">
+              {selectedItemData?.quarters.map((item, i) => (
+                <Card
+                  heading={item.header}
+                  description={item.Description}
+                  number={item.number}
+                  border={false}
+                  key={i}
+                />
               ))}
             </div>
           </div>
-        </Wrapper>
-      </section>
-    );
+          {/* Second Section */}
+          <div className="flex flex-col flex-1 gap-4 gap-x-2 gap-y-4 overflow-hidden">
+            {Data.map((item, i) => (
+              <div key={i} onClick={() => setSelectedItem(item.slug)} className="hover:shadow-xl cursor-pointer">
+                <Program title={item.header} program={item.title} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </Wrapper>
+    </section>
+  );
 };
 
 export default SpecializedTracks;
